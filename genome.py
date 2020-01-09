aaec12aade463f03dbcc7c2ce69f421be571ac8e
Sourabh Sharma <lethaltranquillizer@gmail.com>
2020-01-09 14:11:50 +0530

    KNN impute and l1 regularise

--- genome.py
+++ genome.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import KNNImputer
 from sklearn.model_selection import  train_test_split
 from sklearn.linear_model import LogisticRegression
+
+import seaborn as sns
 #%%
 gct = parse('PAAD.gct')
 data = gct.data_df
@@ -39,27 +41,47 @@ print('#neuroendocrine : ', len(nids))
 print(nids)
 #%%
 Y = data.columns.tolist()
-for yi, id_ in enumerate(Y):
-    Y[yi] = 0
+for i, id_ in enumerate(Y):
+    Y[i] = 0
     if id_ in nids:
-       Y[yi] = 1
-np.array(Y)
-# %%
-X = data.to_numpy().transpose()
-X = SimpleImputer(strategy='constant', fill_value=0).fit_transform(X)
-X = StandardScaler().fit(X).transform(X)
-# %%
-X, x, Y, y = train_test_split(X, Y, test_size=0.2, random_state=42)
-print(x, y)
-lr = LogisticRegression(penalty='l1')
-lr.fit(X, Y)
-Xd = lr.decision_function(X)
-import seaborn as sns
-sns.lineplot(data=Xd)
+       Y[i] = 1
+Y = np.array(Y)
 #%%
-sns.lineplot(data=list(lr.coef_))
-print(lr.score(x,y))
+# missing gene values per sample
+missing = data.shape[0] -  data.count()
+print(missing.describe())
+sns.distplot(missing)
+#%%
+# transpose data
+data = data.T
+#%%
+# look for missing nan values for each gene
+missing = data.shape[0] -  data.count()
+print(missing.describe())
+sns.distplot(missing)
+#%%
+# Using KNN based imputer for missing values ref: https://academic.oup.com/bib/article/12/5/498/268546
+# X = SimpleImputer(strategy='constant', fill_value=0).fit_transform(X)
+X = data.to_numpy()
+X = KNNImputer(n_neighbors=5).fit_transform(X)
+X = StandardScaler().fit_transform(X)
+#%%
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+print(y_test)
+#%%
+# # of neoendocrine samples in test data 
+print('# of neoendocrine samples in test data :', y_test.sum())
+#%%
+lr = LogisticRegression(penalty='l2')
+lr.fit(x_train, y_train)
+sns.lineplot(data=lr.decision_function(x_test))
+#%%
+print(lr.score(x_test, y_test))
 # %%
-for col in data.columns:
-    if col in nids:
-        print(data[col].isna().value_counts())
+sns.lineplot(data=list(lr.coef_))
+#%%
+lr = LogisticRegression(penalty='l1', solver='liblinear')
+lr.fit(x_train, y_train)
+lr.score(x_test, y_test)
+sns.lineplot(data=list(lr.coef_))
+# %%
