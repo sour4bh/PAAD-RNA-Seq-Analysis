70473cb68412f9b7b0e3a45abeb908b4ccf4d76d
Sourabh Sharma <lethaltranquillizer@gmail.com>
2020-01-09 13:05:06 +0530

    lr fit

--- genome.py
+++ genome.py
@@ -2,11 +2,14 @@
 import pandas as pd
 import numpy as np
 from cmapPy.pandasGEXpress.GCToo import GCToo
+
 from cmapPy.pandasGEXpress.parse import parse
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import  train_test_split
+from sklearn.linear_model import LogisticRegression
 #%%
 gct = parse('PAAD.gct')
-#%%
-
 data = gct.data_df
 meta_col = gct.col_metadata_df
 meta_row = gct.row_metadata_df
@@ -35,4 +38,28 @@ for hist_other, other_meta in meta_col.groupby('histological_type_other'):
 print('#neuroendocrine : ', len(nids))
 print(nids)
 #%%
-
+Y = data.columns.tolist()
+for yi, id_ in enumerate(Y):
+    Y[yi] = 0
+    if id_ in nids:
+       Y[yi] = 1
+np.array(Y)
+# %%
+X = data.to_numpy().transpose()
+X = SimpleImputer(strategy='constant', fill_value=0).fit_transform(X)
+X = StandardScaler().fit(X).transform(X)
+# %%
+X, x, Y, y = train_test_split(X, Y, test_size=0.2, random_state=42)
+print(x, y)
+lr = LogisticRegression(penalty='l1')
+lr.fit(X, Y)
+Xd = lr.decision_function(X)
+import seaborn as sns
+sns.lineplot(data=Xd)
+#%%
+sns.lineplot(data=list(lr.coef_))
+print(lr.score(x,y))
+# %%
+for col in data.columns:
+    if col in nids:
+        print(data[col].isna().value_counts())
